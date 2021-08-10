eaa682809461e481c147e37bbefb21836bb60a20
leoEspin <35843183+leoEspin@users.noreply.github.com>
2021-08-10 15:24:51 -0400

    more cases

--- test.py
+++ test.py
@@ -8,6 +8,11 @@ class testito(unittest.TestCase):
         self.assertTrue(pretty(532) == '532')
         self.assertTrue(pretty(1123456789) == '1.1B')
         self.assertTrue(pretty(9487634567534) == '9.5T')
+        
+    def test_other(self):
+        self.assertIsNone(pretty('any'))
+        self.assertTrue(pretty(1000001) == '1M')
+        self.assertTrue(pretty(999999.9) == '999999.9')
 
 if __name__ == '__main__':
     unittest.main()
